55afde6cf3c1c40528e42a1a94ce73db8876a546
jazmindestefanogm <jazmin.destefano@gm2dev.com>
2025-07-16 08:56:00 -0300

    fet: add tag color to stories and test

--- src/components/Tag/Tag.stories.tsx
+++ src/components/Tag/Tag.stories.tsx
@@ -12,5 +12,6 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     label: 'Example Tag',
+    color: '#E1EE32',
   },
 };
--- src/components/Tag/Tag.test.tsx
+++ src/components/Tag/Tag.test.tsx
@@ -4,19 +4,19 @@ import { Tag } from './Tag';
 
 describe('Tag', () => {
   it('renders correctly', () => {
-    render(<Tag label="Test Tag" />);
+    render(<Tag label="Test Tag" color="#E1EE32" />);
     const tagElement = screen.getByText('Test Tag');
     expect(tagElement).toBeInTheDocument();
   });
 
   it('displays the correct label', () => {
     const testLabel = 'Example Label';
-    render(<Tag label={testLabel} />);
+    render(<Tag label={testLabel} color="#E1EE32" />);
     expect(screen.getByText(testLabel)).toBeInTheDocument();
   });
 
   it('has the correct styling', () => {
-    render(<Tag label="Styled Tag" />);
+    render(<Tag label="Styled Tag" color="#E1EE32" />);
     const tagElement = screen.getByText('Styled Tag');
     expect(tagElement).toHaveClass('bg-[#E1EE32]');
     expect(tagElement).toHaveClass('rounded-full');
