85e3e1eb735b0f9fa84dcb9d5686d551ba5bcae8
jazmindestefanogm <jazmin.destefano@gm2dev.com>
2025-07-16 08:53:16 -0300

    feat: add color prop to tag

--- src/components/Tag/Tag.tsx
+++ src/components/Tag/Tag.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 
 export interface TagProps {
   label: string;
+  color: string;
 }
 
-export const Tag = ({ label }: TagProps) => {
+export const Tag = ({ label, color }: TagProps) => {
   return (
-    <span className="inline-block bg-[#E1EE32] text-gray-800 px-3 py-1 rounded-full">
+    <span className={`inline-block bg-[${color}] text-gray-800 px-3 py-1 rounded-full`}>
       {label}
     </span>
   );
